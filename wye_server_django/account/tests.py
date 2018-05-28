624962262120aa18321b3b39ccb1475873942a69
romgar <romain.garrigues.cs@gmail.com>
2018-05-28 23:15:29 +0100

    Add first test to cover create user mutation

--- wye_server_django/account/tests.py
+++ wye_server_django/account/tests.py
@@ -1,3 +1,30 @@
+import urllib
+
 from django.test import TestCase
 
-# Create your tests here.
+from account.models import WYEUser
+
+
+class WYEUserCreation(TestCase):
+
+    def setUp(self):
+        self.graph_url = '/graphql'
+
+    def test_send_create_user_mutation_successfully_creates_a_user(self):
+        mutation = """
+            mutation {
+              createUser(email: "romain@wye.com", pseudo: "Romain", password: "pass") {
+                user {
+                  email,
+                  pseudo
+                }
+              }
+            }
+        """
+        encodedMutation = urllib.parse.quote(mutation)
+        print(encodedMutation)
+        response = self.client.get(self.graph_url, {'mutation': encodedMutation})
+
+        print(response.content)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(WYEUser.objects.count(), 1)
