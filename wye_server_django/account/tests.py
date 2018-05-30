6db7c9ca58e5116a947b8d4d840cb20def6b31c6
romgar <romain.garrigues.cs@gmail.com>
2018-05-30 23:16:03 +0100

    Add test to check that the createUser mutation properly creates an object in the db

--- wye_server_django/account/tests.py
+++ wye_server_django/account/tests.py
@@ -8,10 +8,10 @@ from account.models import WYEUser
 class WYEUserCreation(TestCase):
 
     def setUp(self):
-        self.graph_url = '/graphql'
+        self.graph_url = '/graphql/'
 
     def test_send_create_user_mutation_successfully_creates_a_user(self):
-        mutation = """
+        mutation = '''
             mutation {
               createUser(email: "romain@wye.com", pseudo: "Romain", password: "pass") {
                 user {
@@ -20,11 +20,14 @@ class WYEUserCreation(TestCase):
                 }
               }
             }
-        """
-        encodedMutation = urllib.parse.quote(mutation)
-        print(encodedMutation)
-        response = self.client.get(self.graph_url, {'mutation': encodedMutation})
+        '''
+        query='''
+            query TestQuery { test }
+            mutation TestMutation { writeTest { test } }
+        '''
 
-        print(response.content)
+        response = self.client.post(self.graph_url, {'query': mutation})
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(WYEUser.objects.count(), 1)
+        self.assertEqual(response.content, b'{"data":{"createUser":{"user":{"email":"romain@wye.com","pseudo":"Romain"}}}}')
+        self.assertTrue(WYEUser.objects.filter(email="romain@wye.com", pseudo="Romain").exists())
