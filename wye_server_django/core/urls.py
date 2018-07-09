24a688ae7686458d3434d0c21833cd0423cb5b1f
romgar <romain.garrigues.cs@gmail.com>
2018-07-09 23:00:59 +0100

    Introduce django-cors-headers to handle CORS, as the FE needs cookies coming from the BE and are hosted on a different domain.

--- wye_server_django/core/urls.py
+++ wye_server_django/core/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 from graphene_django.views import GraphQLView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
