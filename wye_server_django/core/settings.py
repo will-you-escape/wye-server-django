b051c4310dbcd5280a918c45fe3fc12436c68e02
romgar <romain.garrigues.cs@gmail.com>
2019-12-08 20:11:08 +0000

    Moves the WhiteNoise middleware after the Django Security one

--- wye_server_django/core/settings.py
+++ wye_server_django/core/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
